ebfcfe7b7d03a6d0cf4c55dfcc74f9c563053cd7
Ítalo Andrade <italodeandra@gmail.com>
2020-04-03 16:36:56 +0200

    fix symmetry of menu button

--- src/views/App/Drawer/AppDrawer.tsx
+++ src/views/App/Drawer/AppDrawer.tsx
@@ -91,7 +91,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     toolbar: {
         ...toolbarStyles(theme),
         // borderBottom: '1px solid rgba(0, 0, 0, 0.12)'
-        paddingRight: 0
+        // paddingRight: 0
     },
 }))
 
