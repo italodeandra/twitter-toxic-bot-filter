7c2c98665b1e57668022c0f555a865bfc0b58caf
Ítalo Andrade <italodeandra@gmail.com>
2020-04-05 19:08:29 +0200

    default separator

--- src/utils/getInitials.ts
+++ src/utils/getInitials.ts
@@ -1,5 +1,5 @@
-export default function getInitials(fullName: string) {
-    let splitName = fullName.split(' ').slice(0, 2)
+export default function getInitials(fullName: string, separator = ' ') {
+    let splitName = fullName.split(separator).slice(0, 2)
     let splitInitials = splitName.map(n => n.charAt(0))
     return splitInitials.join('')
 }
