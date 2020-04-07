97f01a5eb1ebcc507c5f948ed18e4f328b50af20
Ítalo Andrade <italodeandra@gmail.com>
2020-04-07 17:30:31 +0200

    separate dev and prod start and using envvars on config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export default {
-    apiHost: 'http://192.168.17.103:3001',
-    twitterConsumerApiKey: 'kCkSebfJEIEx5ZPo7I7yJPub5',
-    twitterConsumerApiSecretKey: '3coFZCfs1mjzBICWuO20NwsLEgnkYqSOTPQDTmUQPBTnd0LYYA',
+    apiHost: process.env.REACT_APP_API_HOST || 'http://192.168.17.103:3001',
+    apiSocket: process.env.REACT_APP_API_SOCKET || 'ws://192.168.17.103:3001',
+    twitterConsumerApiKey: process.env.REACT_APP_CONSUMER_API_KEY || 'kCkSebfJEIEx5ZPo7I7yJPub5',
+    twitterConsumerApiSecretKey: process.env.REACT_APP_CONSUMER_API_SECRET_KEY || '3coFZCfs1mjzBICWuO20NwsLEgnkYqSOTPQDTmUQPBTnd0LYYA',
     twitterAuthenticateUrl: (oauthToken: string) => `https://api.twitter.com/oauth/authenticate?oauth_token=${oauthToken}`
 }
--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,9 +1,10 @@
 import * as SocketIO from 'socket.io-client'
+import config from '../config'
 
 let socket: SocketIOClient.Socket
 
 export default function useSocket() {
-    if (!socket) socket = SocketIO.connect('ws://localhost:3001')
+    if (!socket) socket = SocketIO.connect(config.apiSocket)
 
     return socket
 }
